acb5b584d7c0c4e2f47ed1ba640c0812ec425617
Jesus Laucho <jlaucho@gmail.com>
2019-01-17 17:00:53 -0400

    ajustando backend

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'name' => $faker->firstName,
+        'apellido' => $faker->lastName,
+        'direccion' => 'Alguna direccion del usuario',
+        'telefono'  => '04165608003',
+        'cedula' => '14136448',
+        'type'  => 'superAdmin',
+        'email' => 'jlaucho@gmail.com',
+        'password' => bcrypt('14460484'), // secret
         'remember_token' => str_random(10),
     ];
 });
